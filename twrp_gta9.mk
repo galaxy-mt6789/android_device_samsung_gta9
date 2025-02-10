3eb5733695f114317fc5ab69bba21e0d37c4583f
Jamie Falconer <mrfluffyoven@gmail.com>
2025-02-10 07:22:13 +0000

    Sync to common tree

--- twrp_gta9.mk
+++ twrp_gta9.mk
@@ -1,21 +1,8 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Release name
+PRODUCT_RELEASE_NAME := gta9
 
 # Inherit from gta9 device
 $(call inherit-product, device/samsung/gta9/device.mk)
-
-#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/gta9/recovery/root,recovery/root)
 
 PRODUCT_DEVICE := gta9
 PRODUCT_NAME := twrp_gta9
@@ -23,4 +10,4 @@ PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-X115
 PRODUCT_MANUFACTURER := samsung
 
-PRODUCT_GMS_CLIENTID_BASE := android-samsung
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/gta9/recovery/root,recovery/root)
